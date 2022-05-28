1151312001473f38e87dacbd82c1f008b4dbd8f6
Максим Наумов <mnauspb@yandex.ru>
2022-05-28 22:31:46 +0300

    formatted

--- graph.js
+++ graph.js
@@ -1,64 +1,66 @@
 const myGraph = document.getElementById('graph');
 
-const points = ['2022-05-26 00:00','2022-05-26 1:00','2022-05-26 2:00','2022-05-26 3:00','2022-05-26 4:00',
-    '2022-05-26 5:00','2022-05-26 6:00','2022-05-26 7:00','2022-05-26 8:00','2022-05-26 9:00','2022-05-26 10:00',
-    '2022-05-26 11:00','2022-05-26 12:00','2022-05-26 13:00','2022-05-26 14:00','2022-05-26 15:00','2022-05-26 16:00',
-    '2022-05-26 17:00','2022-05-26 18:00','2022-05-26 19:00','2022-05-26 20:00','2022-05-26 21:00','2022-05-26 22:00',
-    '2022-05-26 23:00','2022-05-27 00:00']
-let timestamps=['2022-05-26 12:00', '2022-05-26 13:00', '2022-05-26 14:00', '2022-05-26 15:00'];
+const points = ['2022-05-26 00:00', '2022-05-26 1:00', '2022-05-26 2:00', '2022-05-26 3:00', '2022-05-26 4:00',
+    '2022-05-26 5:00', '2022-05-26 6:00', '2022-05-26 7:00', '2022-05-26 8:00', '2022-05-26 9:00', '2022-05-26 10:00',
+    '2022-05-26 11:00', '2022-05-26 12:00', '2022-05-26 13:00', '2022-05-26 14:00', '2022-05-26 15:00', '2022-05-26 16:00',
+    '2022-05-26 17:00', '2022-05-26 18:00', '2022-05-26 19:00', '2022-05-26 20:00', '2022-05-26 21:00', '2022-05-26 22:00',
+    '2022-05-26 23:00', '2022-05-27 00:00']
+let timestamps = ['2022-05-26 12:00', '2022-05-26 13:00', '2022-05-26 14:00', '2022-05-26 15:00'];
 let predict_points, predict_color;
 let product = [10, 20, 6, 4];
-let plan=100;
-let summary =[];
-let prediction=[];
+let plan = 100;
+let summary = [];
+let prediction = [];
 
-Array.prototype.insert = function (timestamp){
-    let datetime=new Date(timestamp);
-    for(let i = 0; i < timestamps.length; i++){
+Array.prototype.insert = function (timestamp) {
+    let datetime = new Date(timestamp);
+    for (let i = 0; i < timestamps.length; i++) {
         if (datetime < new Date(timestamps[i])) {
-            this.splice(i,0,timestamp);
+            this.splice(i, 0, timestamp);
             return [i, false];
-        } 
-        if (datetime === new Date(timestamps[i])){
+        }
+        if (datetime === new Date(timestamps[i])) {
             return [i, true];
         }
     }
-    this.splice(timestamp.length,0,timestamp);
-    return [timestamp.length-1,false];
+    this.splice(timestamp.length, 0, timestamp);
+    return [timestamp.length - 1, false];
 };
 
 function close_form(button) {
-    let dialog =button.closest('.active_form');
+    let dialog = button.closest('.active_form');
     let form = button.closest('.active_form>form');
-    let inputs=form.querySelectorAll('input');
-    for (let i=0; i<inputs.length; i++){inputs[i].value=''}
+    let inputs = form.querySelectorAll('input');
+    for (let i = 0; i < inputs.length; i++) {
+        inputs[i].value = ''
+    }
     dialog.classList.remove('active_form');
 }
 
-function add_new_point(form){
+function add_new_point(form) {
     let time_input = form.querySelector('[name="time"]');
     let product_input = form.querySelector('[name="product"]');
-    let time = time_input.value.split(":",1), amount = product_input.value;
-    time = '2022-05-26 ' + (Number(time)+1) +':00';
-    time_input.value="";
-    product_input.value="";
+    let time = time_input.value.split(":", 1), amount = product_input.value;
+    time = '2022-05-26 ' + (Number(time) + 1) + ':00';
+    time_input.value = "";
+    product_input.value = "";
     let [index, flag] = timestamps.insert(time);
-    
-    if (flag){
+
+    if (flag) {
         product[index] += amount;
-    } else{
-        product.splice(index,0,Number(amount));
+    } else {
+        product.splice(index, 0, Number(amount));
     }
-    
+
     form.closest('.active_form').classList.remove('active_form');
     data = init_data();
     draw();
 }
 
-function add_new_plan(form){
+function add_new_plan(form) {
     let product_input = form.querySelector('[name="product"]');
-    plan=product_input.value;
-    product_input.value="";
+    plan = product_input.value;
+    product_input.value = "";
     form.closest('.active_form').classList.remove('active_form');
     data = init_data();
     draw();
@@ -80,50 +82,50 @@ function draw() {
 }
 
 function init_data() {
-    prediction=[];
-    predict_points=points;
-    predict_points=predict_points.filter((item)=>(new Date(item)>=new Date(timestamps[timestamps.length-1])));
+    prediction = [];
+    predict_points = points;
+    predict_points = predict_points.filter((item) => (new Date(item) >= new Date(timestamps[timestamps.length - 1])));
     console.log(predict_points);
 
-    for (let i=0; i<predict_points.length; i++){
-        prediction[i]=0;
+    for (let i = 0; i < predict_points.length; i++) {
+        prediction[i] = 0;
     }
 
     let average = 0;
-    for (let i = 0; i < timestamps.length; i++){
-        average+=product[i];
+    for (let i = 0; i < timestamps.length; i++) {
+        average += product[i];
         summary[i] = 0;
-        for(let j = 0; j <= i; j++){
+        for (let j = 0; j <= i; j++) {
             summary[i] += product[j];
         }
     }
-    average/=timestamps.length;
-    
-    for (let i = 0; i < prediction.length; i++){
+    average /= timestamps.length;
+
+    for (let i = 0; i < prediction.length; i++) {
         if (i === 0) {
-            prediction[i]=summary[summary.length-1];
+            prediction[i] = summary[summary.length - 1];
             continue;
         }
-        if (prediction[i] === 0){
-            prediction[i] = prediction[i-1]+average;
+        if (prediction[i] === 0) {
+            prediction[i] = prediction[i - 1] + average;
         }
     }
-    
-    predict_color = Number(plan) < prediction[prediction.length-1]?'green':'brown';
-    
-    console.log(Number(plan) < prediction[prediction.length-1]);
+
+    predict_color = Number(plan) < prediction[prediction.length - 1] ? 'green' : 'brown';
+
+    console.log(Number(plan) < prediction[prediction.length - 1]);
     console.log(plan);
     console.log(prediction);
-    
-    let plan_line ={
-        x: [points[0], points[points.length-1]],
+
+    let plan_line = {
+        x: [points[0], points[points.length - 1]],
         y: [plan, plan],
         name: 'План добычи',
-        fill:'tozeroy',
-        line:{
+        fill: 'tozeroy',
+        line: {
             color: '#4FA8FC',
         },
-        fillcolor:  'rgba(222,240,255,0.5)',
+        fillcolor: 'rgba(222,240,255,0.5)',
         type: 'scatter',
     };
 
@@ -143,9 +145,9 @@ function init_data() {
         x: timestamps,
         y: summary,
         name: 'Добыто (сутки)',
-        line:{
+        line: {
             color: 'violet',
-            width:5
+            width: 5
         },
         marker: {
             color: '#C8A2C8',
@@ -155,25 +157,24 @@ function init_data() {
         }
     };
 
-    let product_per_hour ={
+    let product_per_hour = {
         type: 'bar',
         x: timestamps,
         y: product,
         fill: 'tozeroy',
         fillcolor: 'green',
         name: 'Добыто (час)',
-        marker:{
+        marker: {
             width: 2.5,
             color: '#8EED84',
         }
     };
 
 
-
     return [plan_line, product_per_hour, predict_line, product_summary];
 }
 
-data=init_data();
+data = init_data();
 
 let layout = {
     title: 'Скважина 1-1!',
@@ -183,27 +184,27 @@ let layout = {
         "orientation": "h",
     },
     font: {size: 18},
-    yaxis:{
+    yaxis: {
         title: {
-            text:'Дебит',
+            text: 'Дебит',
         },
-        tickmode:'linear',
+        tickmode: 'linear',
         dtick: 20,
         ticksuffix: ' тыс.м',
-        tick0:0,
+        tick0: 0,
         rangemode: 'tozero',
         automargin: true,
     },
-    xaxis:{
+    xaxis: {
         type: 'date',
-        tickmode:'linear',
+        tickmode: 'linear',
         tick0: '2022-05-26',
-        dtick: 1000*60*60,
+        dtick: 1000 * 60 * 60,
         range: ['2022-05-26', '2022-05-27'],
         fixedrange: true,
         automargin: true,
-        rangebreaks:{
-            bounds:['2022-05-26', '2022-05-27'],
+        rangebreaks: {
+            bounds: ['2022-05-26', '2022-05-27'],
             templateitemname: 'name',
         },
     },
